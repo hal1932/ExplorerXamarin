80b53eb6810b82d75bb6b50a0ca1a5cd056a4671
hal1932 <yuta@mbalocal.local>
2017-03-01 21:41:26 +0900

    add mac project

---
+++ ExplorerXamarin.Cocoa/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace ExplorerXamarin.Cocoa
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override void ViewDidAppear()
+		{
+			base.ViewDidAppear();
+			View.Window.Title = MyClass.Test();
+		}
+
+		public override NSObject RepresentedObject
+		{
+			get
+			{
+				return base.RepresentedObject;
+			}
+			set
+			{
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+	}
+}
